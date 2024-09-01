90a46a12f9b91f202fec7c8ff9801ca04dc20762
Mehedi7242 <mehedi2hasan7@gmail.com>
2024-09-02 00:36:56 +0600

    removed gemini api

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyDhAgxe6KMdxUsKRQyy_6WgDLt0YX-qww8"; // Your API key here
+const API_KEY = " Your API key here"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
